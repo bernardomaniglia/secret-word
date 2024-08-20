88cae49df9e02c748c0b6072e5245f612676009c
Bernardo <bernardomaniglia04@gmail.com>
2024-08-20 09:18:19 -0300

    estruturando componente do jogo

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,33 @@
-import './Game.css';
+import "./Game.css";
 
-const Game = ({verifyLetter}) => {
+const Game = ({ verifyLetter }) => {
   return (
-    <div>
-      <h1>Game </h1>
-      <button onClick={verifyLetter}></button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blackSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra:</p>
+        <form>
+          <input typeof="text" name="letter" maxLength="1" required />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Game
+export default Game;
